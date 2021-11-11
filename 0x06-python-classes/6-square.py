617c1c7b118f2493242bac4a735a091973f9e6d3
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-11-11 19:19:42 +0000

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+'''A script that works with squares.
+'''
+
+
+class Square:
+    '''"A class square.
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        '''Initializes a Square with a given size.
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+        '''
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        '''Retrieves the size of this Square.
+        Returns:
+            int: The size of this Square.
+        '''
+        return self.__size
+
+    @property
+    def position(self):
+        '''Retrieves the position of this Square.
+        Returns:
+            tuple: The position of this Square.
+        '''
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        '''Updates the size of this Square.
+        Args:
+            value (int): The new size of this Square.
+        '''
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    @position.setter
+    def position(self, value):
+        '''Updates the position of this Square.
+        Args:
+            value (tuple): The new position of this Square.
+        '''
+        is_invalid_value = True
+        error_msg = 'position must be a tuple of 2 positive integers'
+        if isinstance(value, tuple):
+            if len(value) == 2:
+                if isinstance(value[0], int) and isinstance(value[1], int):
+                    if value[0] >= 0 and value[1] >= 0:
+                        is_invalid_value = False
+        if is_invalid_value:
+            raise TypeError(error_msg)
+        else:
+            self.__position = value
+
+    def area(self):
+        '''Computes the area of this Square.
+        Returns:
+            int: The area of this Square.
+        '''
+        return self.size ** 2
+
+    def my_print(self):
+        '''Prints a square with '#'.
+        '''
+        if self.size == 0:
+            print('')
+        else:
+            print('{}'.format('\n' * self.position[1]), end='')
+            for i in range(self.size):
+                print('{}{}'.format(' ' * self.position[0], '#' * self.size))
