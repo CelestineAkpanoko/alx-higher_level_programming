6a19feba7cd8686b64a6a532b5ace1b742b6f339
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-11-18 23:34:21 +0000

    Task-102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- Lists and filters the avg of a group of records in a table
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+	WHERE month = 7 OR month = 8
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
