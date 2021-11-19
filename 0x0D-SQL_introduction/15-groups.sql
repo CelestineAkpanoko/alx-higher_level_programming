e42a54f43a70afb8117ebd6b89889d779b994263
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-11-19 00:15:10 +0000

    Task-15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists the number of records within a group in a table
-SELECT score, COUNT(score) AS num FROM second_table
+SELECT score, COUNT(score) AS number FROM second_table
 	GROUP BY score 
-	ORDER BY num DESC;
+	ORDER BY number DESC;
