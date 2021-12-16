07666d992b4fee0208516caa1e5b2c0b1e613857
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-12-16 13:47:47 +0100

    Task-2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,5 +4,9 @@ const argc = process.argv.length;
 if (argc <= 2) {
   console.log('No argument');
 } else {
-  console.log('Argument found');
+  if (argc === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 }
