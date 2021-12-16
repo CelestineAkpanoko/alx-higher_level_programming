b125d33253e835a67710cab50f723dd3a06a4462
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-12-16 14:59:09 +0100

    Task-7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * nPrint - Prints a string n times.
+ * @param {Number} n - Tehe number of times to print the string.
+ * @param {String} txt - The string to be printed.
+ */
+function nPrint (n, txt) {
+  if (Number.isNaN(n)) {
+    console.log('Missing number of occurrences');
+  } else if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+      console.log(txt);
+    }
+  }
+}
+nPrint(Number.parseInt(process.argv[2]), 'C is fun');
