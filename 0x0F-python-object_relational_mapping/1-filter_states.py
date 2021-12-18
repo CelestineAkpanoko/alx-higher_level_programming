ab5b0f57a3b315f41427f51cf9b91b44dcaa6d9f
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-12-18 22:05:50 +0100

    Task-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,21 +4,22 @@
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
     if len(sys.argv) >= 4:
-        db_conn = MySQLdb.connect(
-                host='localhost',
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
-                )
-        cur = db_conn.cursor()
-        cur.execute(
-                'SELECT * FROM states WHERE name IS NOT NULL AND LIKE "N%" ' + 
-                'ORDER BY states.id ASC;'
-                )
-        results = cur.fetchall()
+        db_connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db_connection.cursor()
+        cursor.execute(
+            'SELECT * FROM states WHERE name IS NOT NULL AND' +
+            ' LEFT(CAST(name AS BINARY), 1) = "N" ORDER BY states.id ASC;'
+        )
+        results = cursor.fetchall()
         for result in results:
             print(result)
-        db_conn.close()
+        db_connection.close()
