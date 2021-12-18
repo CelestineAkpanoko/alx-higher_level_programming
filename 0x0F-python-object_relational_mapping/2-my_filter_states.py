6f23d191e8ca9b58ff0ba856a5d6433d48453d0f
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-12-18 22:07:28 +0100

    Task-2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,21 +4,23 @@
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
-    if len(sys.arg)v >= 5:
-        db_conn = MySQLdb.connect(
-                host='localhost',
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
-                )
-        cur = db_conn.cursor()
+    if len(sys.argv) >= 5:
+        db_connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db_connection.cursor()
         state_name = sys.argv[4]
-        cur.execute('SELECT * FROM states WHERE name="{}"' + 
-               ' ORDER BY id ASC;'.format(state_name))
-        results = cur.fetchall()
+        cursor.execute(
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
+        )
+        results = cursor.fetchall()
         for result in results:
             print(result)
-        db_conn.close()
-
+        db_connection.close()
