42a6d30023310bcbec5407bdc0a76e578200e857
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-01-12 10:08:03 +0100

    :white_check_mark: Sends a POST request to given URL and displays the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a given URL and displays the response
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD""$1"
+curl -sL -X POSt -d 'email=test%40gmail.com&subject=I+will+always+be+here+for+PLD' "$1"
