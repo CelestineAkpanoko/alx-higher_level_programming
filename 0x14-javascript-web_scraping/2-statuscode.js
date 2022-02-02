0ce67f38fa178dcf948f29724db8f7b789a27dff
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-02-02 10:33:50 +0100

    A script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const re = require('request');
 
 if (process.argv.length > 2) {
-	re.get(process.argv[2])
-	.on('response', function(response) {
-		console.log('code: ', response.statusCode)
-	})
+  re.get(process.argv[2])
+    .on('response', function (response) {
+      console.log('code: ', response.statusCode);
+    });
 }
