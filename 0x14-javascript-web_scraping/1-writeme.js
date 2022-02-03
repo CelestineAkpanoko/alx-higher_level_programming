6646c6fbf7afced54a66f1502b3f518d4673b82e
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-02-03 10:35:55 +0100

    Completed Task-1, and Tasks-4 to Task-8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,9 @@
 
 const fs = require('fs');
 
-if (process.argv.length > 2) {
-  fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-    if (!err) {
-      console.log(data.toString('utf-8'));
-    } else {
+if (process.argv.length > 3) {
+  fs.writeFile(process.argv[2], process.argv[3], err => {
+    if (err) {
       console.log(err);
     }
   });
